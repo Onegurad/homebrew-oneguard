b3d1c2e3f5a2c28f4269ea0c37b4c0b50431e384
mohmadfarhat <alfaresof@gmail.com>
2026-08-01 18:35:09 +0300

    init

---
+++ Formula/oneguard.rb
@@ -0,0 +1,11 @@
+class Oneguard < Formula
+  desc "OneGuard CLI for managing projects and secrets"
+  homepage "https://oneguard.one"
+  url "https://github.com/Onegurad/homebrew-oneguard/releases/download/0.0.01/oneguard-mac.tar.gz"
+  sha256 "5fb002fff9a86afac531415772da8ac043596caf213da9a975b6120880c34571"
+  version "0.0.01"
+
+  def install
+    bin.install "oneguard"
+  end
+end
